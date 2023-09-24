183d3020a5f69b7e58be85b95f1cd5607688c0ef
sakshi-0009 <sakshigome1809@gmail.com>
2023-09-24 18:42:11 +0530

    Square root of any integer value

--- Problem_Solving_Approach/SquareRoot2.java
+++ Problem_Solving_Approach/SquareRoot2.java
@@ -4,4 +4,20 @@ import java.io.*;
 
 public class SquareRoot2 {
 
+	static int sqrt(int num) {
+		int val = 0;
+		for(int i=1; i<=num; i++) {
+			if(i*i<=num) {
+				val = i;
+			}
+		}
+		return val;
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter Integer value: ");
+		int num = Integer.parseInt(br.readLine());
+		int ret = sqrt(num);
+		System.out.println(ret);
+	}
 }
