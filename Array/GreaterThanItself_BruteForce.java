ae72d47705c546aaaaa09e2160c904312452af00
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-03 01:04:55 +0530

    The no. of elements having at least 1 element greater than itself

---
+++ Array/GreaterThanItself_BruteForce.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.io.*;
+
+public class GreaterThanItself_BruteForce {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter array size : ");
+		int size = Integer.parseInt(br.readLine());
+		int arr[] = new int[size];
+		
+		System.out.println("Enter array elements : ");
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int count = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				if(arr[i]<arr[j]) {
+					count++;
+					break;
+				}
+			}
+		}
+		System.out.println("The no. of elements having at least 1 element greater than itself is : "+count);
+	}
+}
+//Time Complexity : O(N^2)
+//Space Complexity : O(1)
