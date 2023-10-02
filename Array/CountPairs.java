fc28d2b4431c9d27b9db7d15fc597513e93276f9
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-03 01:20:59 +0530

    Count of pairs having addition K (Brute Force)

---
+++ Array/CountPairs.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.io.*;
+
+public class CountPairs {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter array size : ");
+		int size = Integer.parseInt(br.readLine());
+		int arr[] = new int[size];
+		
+		System.out.println("Enter array elements : ");
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		System.out.println("Enter sum : ");
+		int sum = Integer.parseInt(br.readLine());
+		int count = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				if(i!=j) {
+					if(arr[i]+arr[j]==sum) {
+						count++;
+					}
+				}
+			}
+		}
+		System.out.println("Count of Pairs : "+count);
+	}
+}
+//Time Complexity : O(N^2)
+//Space Complexity : O(1)
