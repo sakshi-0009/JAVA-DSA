498832628e533a0c7793208c49c121774139208d
sakshi-0009 <sakshigome1809@gmail.com>
2023-10-04 22:26:07 +0530

    Maximum element upto given range

---
+++ Array/MaxInRange.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.io.*;
+
+public class MaxInRange {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter array size : ");
+		int size = Integer.parseInt(br.readLine());
+		int arr[] = new int[size];
+		
+		System.out.println("Enter array elements : ");
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		System.out.println("Enter range : ");
+		int Range = Integer.parseInt(br.readLine());
+		int max = Integer.MIN_VALUE;
+		for(int i=0; i<=Range; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println("Maximum element upto given range is : "+max);
+	}
+}
